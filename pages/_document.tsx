2231b05017e3d5e7265a0fb6a0a74798d6642f8d
Alina <leesha_@mail.ru>
2023-01-19 15:39:31 +0700

    add fonts

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,13 +1,77 @@
-import { Html, Head, Main, NextScript } from 'next/document'
+import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
   return (
-    <Html lang="en">
-      <Head />
+    <Html>
+      <Head>
+        <link
+          ref="preload"
+          href="fonts/MazzardH-Bold.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin=""
+        />
+        <link
+          ref="preload"
+          href="fonts/MazzardH-Medium.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+        <link
+          ref="preload"
+          href="fonts/MazzardH-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+        <link
+          ref="preload"
+          href="fonts/Move-X-Bold.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin=""
+        />
+        <link
+          ref="preload"
+          href="fonts/NeueMachina-Bold.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+        <link
+          ref="preload"
+          href="fonts/NeueMachina-Medium.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+        <link
+          ref="preload"
+          href="fonts/NeueMachina-Regular.woff2"
+          as="font"
+          type="font/woff2"
+          crossOrigin=""
+        />
+        <link
+          ref="preload"
+          href="fonts/TTOctosquares-Medium.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin=""
+        />
+        <link
+          ref="preload"
+          href="fonts/TTOctosquares-Regular.otf"
+          as="font"
+          type="font/otf"
+          crossOrigin=""
+        />
+      </Head>
       <body>
         <Main />
         <NextScript />
       </body>
     </Html>
-  )
+  );
 }
