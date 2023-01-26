19cfe28541d667e14595bbb348304998c7d85340
Alina <leesha_@mail.ru>
2023-01-26 13:05:57 +0700

    add styles to socialNetworks

--- components/SocialNetworks/index.tsx
+++ components/SocialNetworks/index.tsx
@@ -1,12 +1,46 @@
-import Link from "next/link";
+import cl from "classnames";
 import styles from "./style.module.scss";
 
 interface SocialNetworksProps {}
 
+const socialNetworks = [
+  {
+    id: 1,
+    title: "Discord",
+    className: "Discord",
+    link: "http//facebook.com",
+  },
+  {
+    id: 2,
+    title: "Twitter",
+    className: "Twitter",
+    link: "http//twitter.com",
+  },
+  {
+    id: 3,
+    title: "AntDesing",
+    className: "AntDesing",
+    link: "http//ant.desing.com",
+  },
+];
 export const SocialNetworks: React.FC<SocialNetworksProps> = ({}) => {
   return (
-    <ul>
-      <li>12314</li>
+    <ul className={styles.socialNetworks}>
+      {socialNetworks.map((socialNetwork) => (
+        <li key={socialNetwork.id} className={styles.socialNetworksItem}>
+          <a
+            href={socialNetwork.link}
+            target="_blank"
+            rel="noreferrer"
+            className={cl(
+              styles.socialNetworksLink,
+              styles[`socialNetworks${socialNetwork.className}`]
+            )}
+          >
+            {socialNetwork.title}
+          </a>
+        </li>
+      ))}
     </ul>
   );
 };
